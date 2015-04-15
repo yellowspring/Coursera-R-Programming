e6a390c8bd0c5cecc39ab4e14348062b9a9e4fc4
Michael Zhang <yellowspringroad@gmail.com>
2015-04-15 05:58:19 +0100

    upate

--- Assignment3/best.R
+++ Assignment3/best.R
@@ -3,4 +3,14 @@ best <- function(state, outcome) {
   ## Check that state and outcome are valid
   ## Return hospital name in that state with lowest 30-day death
   ## rate
+  states <- unique(outcome[,7],fromLast = FALSE)
+  if ( ! state %in% states) {
+    stop('invalid state')
+  }
+  
+  if (! outcome %in% c("heart attack", "heart failure", "pneumonia") ) {
+    stop('invalid outcome')
+  }
+  
+  
 }

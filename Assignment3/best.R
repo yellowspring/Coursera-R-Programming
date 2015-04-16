2d99b27daab0b2cf7022cc4fd86603b8ecc4ccef
Michael Zhang <yellowspringroad@gmail.com>
2015-04-16 05:54:04 +0100

    update

--- Assignment3/best.R
+++ Assignment3/best.R
@@ -3,7 +3,8 @@ best <- function(state, outcome) {
   ## Check that state and outcome are valid
   ## Return hospital name in that state with lowest 30-day death
   ## rate
-  states <- unique(outcome[,7],fromLast = FALSE)
+  content <- read.csv('/home/osboxes/workspace/learningR/Coursera-R-Programming/Assignment3/outcome-of-care-measures.csv')
+  states <- unique(content[,7],fromLast = FALSE)
   if ( ! state %in% states) {
     stop('invalid state')
   }
@@ -12,5 +13,18 @@ best <- function(state, outcome) {
     stop('invalid outcome')
   }
   
-  
+  rv <- c()
+  if (outcome == 'heart attack') {
+    rv <- content[content$State == state,][[11]]
+  } else if (outcome == 'heart failure') {
+    rv <- content[content$State == state,][[17]]
+  } else if (outcome == 'pneumonia') {
+    rv <- content[content$State == state,][[23]]
+  }  
+  rv <- suppressWarnings(as.numeric(as.character(rv)))
+  min_val<- min(rv, na.rm=TRUE)
+  index <- match(min_val,rv)
+
+  hospital_names <- as.character(content[[2]])[index]
+  sort(hospital_names)[1]
 }
